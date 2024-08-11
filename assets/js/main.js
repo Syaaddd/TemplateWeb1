6b2f9ad69df831277a95153607a1abf638ed2b31
Arsyad2009 <105086466+Arsyad2009@users.noreply.github.com>
2024-08-11 07:04:45 +0700

    Deskripsi perubahan

--- assets/js/main.js
+++ assets/js/main.js
@@ -178,6 +178,12 @@
   document.addEventListener('scroll', navmenuScrollspy);
 })();
 
+
+
+/**MyCode */
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
   const images = document.querySelectorAll('.about-images img');
 
@@ -192,3 +198,26 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
+
+/**
+* Template Name: Bootslander
+* Template URL: https://bootstrapmade.com/bootslander-free-bootstrap-landing-page-template/
+* Updated: Aug 07 2024 with Bootstrap v5.3.3
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+
+(function() {
+  "use strict";
+
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+})();
